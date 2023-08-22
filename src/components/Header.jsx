8e10ba1ed80384dc7f49b8dfbc791269143f53c6
BIROUE ISAAC <biroueisaac@gmail.com>
2023-08-22 09:45:24 +0100

    refactor my code by adding fontawesome dependencies and modifiying the header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,16 +1,24 @@
 import "../../src/App.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faCartShopping } from "@fortawesome/free-solid-svg-icons";
 function Header() {
   return (
-    <div className="flex items-center gap-x-20 px-5 py-10">
+    <div className="flex items-center gap-x-20 px-5 py-10 justify-center">
       <div className="font-extrabold text-2xl">sneakers</div>
-      <div>
-        <ul className="flex gap-x-5 items-center text-[#e8e7eb] text-xl font-extrabold">
-          <li>Collection</li>
-          <li>Men</li>
-          <li>Women</li>
-          <li>About</li>
-          <li>Contact</li>
-        </ul>
+      <div className="flex gap-x-20 items-center">
+        <div>
+          <ul className="flex gap-x-5 items-center text-[#e8e7eb] text-xl font-extrabold">
+            <li>Collection</li>
+            <li>Men</li>
+            <li>Women</li>
+            <li>About</li>
+            <li>Contact</li>
+          </ul>
+        </div>
+        <div className="flex gap-x-5 items-center">
+          <FontAwesomeIcon icon={faCartShopping} />
+          <img src="" alt="userimage" />
+        </div>
       </div>
     </div>
   );
